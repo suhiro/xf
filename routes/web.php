d57793385ac2426222dfdeebfc5644decb16cbeb
Hiro <suhiro@gmail.com>
2017-08-17 21:30:07 -0400

    fix commit

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
-// Route::get('/', function () {
+Route::get('/', function () {
 
-// 	$events = DB::table('lot_events')->get();
-//    // return view('welcome',compact('events'));
-// 	return view('master.layout');
-// });
-
-<<<<<<< HEAD
-	$events = DB::table('lot_events')->get();
-    return view('welcome',compact('events'));
-	//return "hello";
+	// $events = DB::table('lot_events')->get();
+ //   // return view('welcome',compact('events'));
+	return view('master.layout');
 });
-=======
-Route::get('/','LogController@index');
->>>>>>> 9461084b77fc4da61fa69c66f9287dcdd16e57a0
+
+
