ea0fc65e0943159f765ccf08951dd232892a3362
Hiro <suhiro@gmail.com>
2017-10-03 00:37:14 -0400

    asset file location bug
    the public folder, remember

--- resources/views/layouts/assets.blade.php
+++ resources/views/layouts/assets.blade.php
@@ -2,7 +2,7 @@
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet">
 
 <!-- Bootstrap core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="{{URL::asset('css/bootstrap.min.css')}}" rel="stylesheet">
 <!-- Custom styles for this template -->
 <link href="{{URL::asset('assets/global/plugins/font-awesome/css/font-awesome.min.css')}}" rel="styleseet">
 <link href="{{URL::asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css')}}" rel="styleseet">
@@ -10,5 +10,5 @@
 <link href="{{URL::asset('assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css')}}" rel="styleseet">
 <!-- END GLOBAL MANDATORY STYLES -->
 <link href="{{URL::asset('assets/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css')}}" rel="stylesheet">
-<link href="css/dashboard.css" rel="stylesheet">
+<link href="{{URL::asset('css/dashboard.css')}}" rel="stylesheet">
 
