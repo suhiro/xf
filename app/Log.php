da6345625b3cc04799a96694e0877ec120313c3f
Hiro <suhiro@gmail.com>
2018-01-26 22:29:02 -0500

    progress1

--- app/Log.php
+++ app/Log.php
@@ -49,4 +49,55 @@ class Log extends Model
        return $muba;
 
     }
+
+
+    public static function getMUBA3($dayOutput,$date,$serial,$interval){
+        // $dayOutput = $this->getDailyOutputByMachine($date,$serial);
+          $firstRow = DB::table('event_logs')->join('errors','event_logs.ERR_event','errors.err_code')->
+                    where('serial',$serial)->
+                    whereDate('event_logs.MCGS_Time',$date)->first();
+        
+          $assists = self::assistCodes($serial,$date);
+        
+          $totalErrors = 0;
+          $debugArray = array();
+   
+          $startMark = $firstRow->output;
+          $endMark = $firstRow->output + $interval;
+          $errorArray = array();
+          foreach($assists as $a){
+            if($a->output >= $startMark && $a->output < $endMark){
+                array_push($errorArray, $a->ERR_event);
+            } else {
+                $startMark = $a->output;
+                $endMark = $startMark + $interval;
+
+                array_push($debugArray,$errorArray);
+                $totalErrors += sizeof(array_unique($errorArray));
+                $errorArray = array();
+                array_push($errorArray, $a->ERR_event);
+
+
+            }
+          }
+
+
+        $result = (object)'';
+        if($totalErrors == 0) { // in case total errors is zero
+            $totalErrors = 1;}
+        $result->muba = round($dayOutput/$totalErrors,2);
+        $result->assists =  $totalErrors;
+       return $result;
+
+    }
+
+    public static function assistCodes($serial,$date)
+    {
+      return DB::table('event_logs')->join('errors','event_logs.ERR_event','errors.err_code')->
+                    select('ERR_event','output')->
+                    where('serial',$serial)->
+                    whereDate('event_logs.MCGS_Time',$date)->get();
+    }
+
+
 }
